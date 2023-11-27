7065062059da9688c136398b9b5f387b3a630d88
jakubkrzywda <qbakrzywda@gmail.com>
2023-11-27 20:23:06 +0100

    Made changes in edit page template in order to display WYSIWYG editor.

--- CoolSchool/articles_app/forms.py
+++ CoolSchool/articles_app/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Article
+from ckeditor.widgets import CKEditorWidget
 
 
 class ArticleForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+
     class Meta:
         model = Article
-        fields = ['title', 'content', 'page']
+        fields = ['title', 'page']
--- CoolSchool/articles_app/models.py
+++ CoolSchool/articles_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 
 class Page(models.Model):
     title = models.CharField(max_length=100)
@@ -10,6 +10,6 @@ class Page(models.Model):
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = RichTextField()
     pub_date = models.DateTimeField('date published')
     page = models.ForeignKey(Page, on_delete=models.CASCADE, null=True)
